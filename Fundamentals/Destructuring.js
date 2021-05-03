fae4d3016447be09001645e592e4af8b8b9db5a4
pdavis213 <78828994+pdavis213@users.noreply.github.com>
2021-05-03 13:49:34 -0400

    destructuring

---
+++ Fundamentals/Destructuring.js
@@ -0,0 +1,64 @@
+1.
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+// log tesla
+// log mercedes
+
+2.
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+// console log of name will result in an error as name is no longer defined as it is appended to otherName
+
+3.
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+// logs 12345
+// logs undefined as hashedPassword only exists inside person object and needs to be called differently in order to be accessed 
+
+4.
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+// logs false
+// logs true
+
+5.
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+// logs 'value'
+// logs [1,5,1,8,3,3]
+// logs 1
+// logs 5
