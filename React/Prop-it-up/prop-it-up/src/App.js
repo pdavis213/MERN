3f9172d21ac6c0ed86644bbf0d552629e37060f4
pdavis213 <78828994+pdavis213@users.noreply.github.com>
2021-05-04 17:09:35 -0400

    revamp

--- React/Prop-it-up/prop-it-up/src/App.js
+++ React/Prop-it-up/prop-it-up/src/App.js
@@ -5,20 +5,11 @@ import NewComponent from './components/NewComponent';
 
 function App() {
   return (
-    <div className="App">
-      <NewComponent name = {<h1>"Doe, Jane"</h1>}/>
-      <NewComponent age = {<p>Age: 45</p>}/>
-      <NewComponent hairColor = {<p>Hair Color: Black</p>}/>
-      <NewComponent name = {<h1>"Smith, John"</h1>}/>
-      <NewComponent age = {<p>Age: 88</p>}/>
-      <NewComponent hairColor = {<p>Hair Color: Brown</p>}/>
-      <NewComponent name = {<h1>"Fillmore, Millard"</h1>}/>
-      <NewComponent age = {<p>Age: 50</p>}/>
-      <NewComponent hairColor = {<p>Hair Color: Brown</p>}/>
-      <NewComponent name = {<h1>"Smith, Maria"</h1>}/>
-      <NewComponent age = {<p>Age: 62</p>}/>
-      <NewComponent hairColor = {<p>Hair Color: Brown</p>}/>
-
+    <div>
+      <NewComponent last_name = {'Doe'} first_name = {'Jane'} age = {45} hairColor = {'Black'}/>
+      <NewComponent last_name = {'Smith'} first_name = {'John'} age = {88} hairColor = {'Brown'}/>
+      <NewComponent last_name = {'Fillmore'} first_name = {'Millard'} age = {50} hairColor = {'Brown'}/>
+      <NewComponent last_name = {'Smith'} first_name = {'Maria'} age = {62} hairColor = {'Brown'}/>
     </div>
   );
 }
--- React/Prop-it-up/prop-it-up/src/components/NewComponent.js
+++ React/Prop-it-up/prop-it-up/src/components/NewComponent.js
@@ -4,9 +4,9 @@ class NewComponent extends Component{
     render(){
         return(
             <div>
-                {this.props.name}
-                {this.props.age}
-                {this.props.hairColor}
+                <h1>{this.props.last_name}, {this.props.first_name}</h1>
+                <p>Age: {this.props.age}</p>
+                <p>Hair Color: :this.props.hairColor</p>
             </div>
         );
     }
