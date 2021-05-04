b3a2e6d04a4da154c0fa05d8c693a7be621074aa
pdavis213 <78828994+pdavis213@users.noreply.github.com>
2021-05-04 17:10:01 -0400

    fix

--- React/Prop-it-up/prop-it-up/src/components/NewComponent.js
+++ React/Prop-it-up/prop-it-up/src/components/NewComponent.js
@@ -6,7 +6,7 @@ class NewComponent extends Component{
             <div>
                 <h1>{this.props.last_name}, {this.props.first_name}</h1>
                 <p>Age: {this.props.age}</p>
-                <p>Hair Color: :this.props.hairColor</p>
+                <p>Hair Color: {this.props.hairColor}</p>
             </div>
         );
     }
