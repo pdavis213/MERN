8078c8ed8dd9b844564b4d9fab81d3d7ba1f4892
pdavis213 <78828994+pdavis213@users.noreply.github.com>
2021-05-04 15:38:23 -0400

    updates

--- React/my-first-react-app/src/App.js
+++ React/my-first-react-app/src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import MyNewComponent from './components/MyNewComponent';
 
 function App() {
   return (
-    <div>
-        <h1>
-          Hello World!
-        </h1>
-        <h2>Things I need to do:</h2>
-        <ul>
-          <li>Learn React</li>
-          <li>Climb Mt. Everest</li>
-          <li> Run a marathon</li>
-          <li>Feed the dogs</li>
-        </ul>
+    <div className = "App">
+      <MyNewComponent />
     </div>
   );
 }
---
+++ React/my-first-react-app/src/components/MyNewComponent.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class MyNewComponent extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello World!</h1>
+                <h2>Things I need to do:</h2>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li> Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default MyNewComponent;
