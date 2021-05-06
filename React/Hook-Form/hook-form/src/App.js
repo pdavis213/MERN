9e8ed16b2cb6362f8cca5c8b0192fae81d8eafe2
pdavis213 <78828994+pdavis213@users.noreply.github.com>
2021-05-06 16:35:27 -0400

    hook-form commit

---
+++ React/Hook-Form/hook-form/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Result from './components/Result';
+import Form from './components/Form';
+import './App.css';
+
+function App() {
+
+  const[ state, setState ] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  }); 
+  return (
+    <div className="App">
+      <Form inputs={state} setInputs={setState}/>
+      <Result data={state} />
+    </div>
+  );
+}
+
+export default App;
