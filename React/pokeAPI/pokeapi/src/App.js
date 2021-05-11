18b97b2dac121172dda088d9c4bd1bd988d33707
pdavis213 <78828994+pdavis213@users.noreply.github.com>
2021-05-11 14:28:44 -0400

    pokeapi commit

---
+++ React/pokeAPI/pokeapi/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from 'react';
+
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  //test comment
+  const fetchPokemon = (event) => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        setPokemon(res.results);
+      })
+      .catch(err => console.log(err))
+  }
+
+  return (
+    <div className="App">
+      <button className="btn btn-danger btn-lg mt-5" onClick={fetchPokemon}>Fetch Pokemon</button>
+
+      {
+        pokemon.map((item, i)=><h1>{item.name}</h1>)
+      }
+
+    </div>
+  );
+}
+
+export default App;
