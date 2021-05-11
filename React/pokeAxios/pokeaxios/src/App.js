dc48e4d2295f7b628702e9941dba2424461ecff2
pdavis213 <78828994+pdavis213@users.noreply.github.com>
2021-05-11 14:57:06 -0400

    pokeApi initial commit

---
+++ React/pokeAxios/pokeaxios/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+    .then(res=>{
+      setPokemon(res.data.results);
+    })}, []);
+
+  return (
+    <div className="App">
+      <button className="btn btn-warning btn-lg mt-5" onClick="">Fetch Pokemon</button>
+
+      {
+        pokemon.map((item, i)=><h1>{item.name}</h1>)
+      }
+
+    </div>
+  );
+}
+
+export default App;
