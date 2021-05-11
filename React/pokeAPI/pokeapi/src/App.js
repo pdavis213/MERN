ad78d0d911d2e5e9458953186a592dfb4436bbd1
pdavis213 <78828994+pdavis213@users.noreply.github.com>
2021-05-11 14:30:06 -0400

    commit

--- React/pokeAPI/pokeapi/src/App.js
+++ React/pokeAPI/pokeapi/src/App.js
@@ -5,7 +5,6 @@ import {useState} from 'react';
 function App() {
   const [pokemon, setPokemon] = useState([]);
 
-  //test comment
   const fetchPokemon = (event) => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
       .then(res => res.json())
